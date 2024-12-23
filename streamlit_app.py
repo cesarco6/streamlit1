fcb696af9c9a3d37985709223161a8bd0837d8bd
Alfredo Cesar Cortes Olivares <81404940+cesarco6@users.noreply.github.com>
2024-12-23 17:37:14 +0000

    Translation to sp y botones dinamicos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Mi nueva app en spanich")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+cantidad = st.slider('Selecciona la cantidad')
+st.write(f'la cantidad es: {cantidad}')
+
+for i in range(cantidad):
+      st.button(f'{i}')
